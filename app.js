5687e9114185ea0ebe8b43ea710e6b4f2a29341e
haydn paterson <haydn.developer@gmail.com>
2013-07-02 02:51:42 +1200

    commit

---
+++ app.js
@@ -0,0 +1,17 @@
+﻿require('typescript.api').register();
+
+ 
+var appex = require('appex');
+
+var server = appex.createServer(__dirname + '/service.ts');
+
+server.listen(1234);
+
+
+
+
+ 
+
+ 
+
+
---
+++ service.ts
@@ -0,0 +1,20 @@
+﻿var fs = require('fs')
+
+export module services
+{   
+    export class Service
+    {
+        constructor() 
+        {
+            console.log('Service 1')
+
+        }
+        
+        public method(data:string) : string {
+
+            return data;
+
+        }
+    }
+}
+ 
