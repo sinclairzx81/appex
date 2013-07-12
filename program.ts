2a5ad8c8a71239342c78575bec917f8ea9f7f99e
haydn paterson <haydn.developer@gmail.com>
2013-07-12 19:15:19 +1200

    adding middleware

--- program.ts
+++ program.ts
@@ -1,12 +1,41 @@
 ﻿/// <reference path="node_modules/appex/appex.d.ts" />
 
+function authenicate(context:appex.web.IContext){
+
+    console.log('authenicate');
+
+    context.next();
+}
+
+function authorize(context:appex.web.IContext) {
+
+    console.log('authorize');
+
+    context.next();
+}
+
+cascade({ use : [ authenicate, authorize ] })
+
 export function index (context:appex.web.IContext) { 
-    
-    context.response.send('hello');
+        
+    console.log(context.cascade);
+
+    context.response.send('index')
 
 }
 
+cascade('about', { use : [ ] })
+export function about (context:appex.web.IContext) { 
+        
+    console.log(context.cascade);
+
+    context.response.send('index')
+
+} 
+
 export function wildcard(context, path) {
+
+    console.log(context.cascade)
 
     context.response.serve('./', path);
 }
