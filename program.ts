325a5ab5f888af0806225d6ade45c6d23ea7f32d
haydn paterson <haydn.developer@gmail.com>
2013-07-15 16:10:14 +1200

    commit

--- program.ts
+++ program.ts
@@ -3,33 +3,15 @@
 
 export module models {
 
-	export class Address {
-
-		public addressLine1: string;
-		
-        public addressLine2: string;
-		
-        public addressLine3: string;
-	}
-	
-	export class User {
-		
-        public id : string;
-	}
-
-	export class Customer extends User {
-
-        public firstname : string;
-
-        public address   : Address[];
-
-        public friend : Customer[];
+	export class Node {
+            
+        public child_nodes: Node[];
 	}
 }
 
 export function index (context:appex.web.IContext) {
 
-    context.response.json( context.schema.get('models.Customer') );
+    context.response.json( context.schema.get('models.Node') );
 }
 
 
