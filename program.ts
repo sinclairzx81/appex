a72802c4a39085a71197b3685e04373ea570f856
haydn paterson <haydn.developer@gmail.com>
2013-07-17 00:51:52 +1200

    update to tsapi 0.5.9

--- program.ts
+++ program.ts
@@ -1,10 +1,56 @@
 ﻿/// <reference path="node_modules/appex/appex.d.ts" />
 /// <reference path="studio/index.ts" />
 
+export module models {
+
+    export class Address {
+
+        /** street */
+        public addressLine1: string;
+        /** suburb */       
+        public addressLine2: string;
+    }
+
+    export class User {
+
+        /** this users id */
+        public id : string;
+    }
+
+    export class Customer extends User {
+
+        /** the customers firstname */
+        public firstname  : string;
+
+        /** the customers lastname */
+        public lastname   : string;
+    }
+
+    export class Employee extends User {
+
+        /** the employees firstname */
+        public firstname  : string;
+
+        /** the employees lastname */
+        public lastname   : string;
+
+        /** the employees address */
+        public address  : Address;
+
+        /** this employees customers */
+        public customers : Customer[];
+    }
+}
+
 export function index (context:appex.web.IContext) {
 
     context.response.send('hello.');
 
+}
+
+export function schema(context:appex.web.IContext) {
+
+    context.response.json(context.schema.get('models.Employee'));
 }
 
 
