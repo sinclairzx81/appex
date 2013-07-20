21a06879c404b3628df692e4a8d4f977421849e0
haydn paterson <haydn.developer@gmail.com>
2013-07-20 19:55:05 +1200

    improved validation of json schema.

--- program.ts
+++ program.ts
@@ -4,15 +4,14 @@
 
 interface Address {
 
-    line1     : string;
-    address   : Address;
+    line1    : string;
+    addresses:Address[];
 }
 
 class Customer {
 
-    public address   : Address;
+    public address   : Address[];
 }
-
 
 export function index (context:appex.web.IContext) {
 
@@ -20,29 +19,31 @@ export function index (context:appex.web.IContext) {
 
     var handle:any = customer;
 
-    handle.address = {};
-    handle.address.line1 = 'a';
-    handle.address.address = {};
-    handle.address.address.line1 = ' ';
-
-
-
-    var schema = context.schema.get('Customer');
-
-    var errors = context.schema.validate('Customer', customer);
-
-    var output = JSON.stringify(customer, null, 4) + '\n';
-
-    output += '-------------------------------\n';
-
-    output += JSON.stringify(schema, null, 4) + '\n';
-
-    output += '-------------------------------\n';
     
-    output += JSON.stringify(errors, null, 4) + '\n';
+    handle.address = [{line1:'a', addresses:[1]}];
 
+
+
+
+
+
+
+
+
+
+
+
+    //---------------------------------------------
+    // output
+    //---------------------------------------------
+    var schema = context.schema.get('Customer');
+    var errors = context.schema.validate('Customer', customer);
+    var output = JSON.stringify(customer, null, 4) + '\n';
     output += '-------------------------------\n';
-
+    output += JSON.stringify(schema, null, 4) + '\n';
+    output += '-------------------------------\n';
+    output += JSON.stringify(errors, null, 4) + '\n';
+    output += '-------------------------------\n';
     context.response.send(output);
 }
 
