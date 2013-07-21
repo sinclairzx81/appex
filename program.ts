ff76e9d1b256e07f86e71b47fbd1f62cbfa497bf
haydn paterson <haydn.developer@gmail.com>
2013-07-22 04:27:24 +1200

    fixing require paths.

--- program.ts
+++ program.ts
@@ -1,46 +1,7 @@
 ﻿/// <reference path="node_modules/appex/appex.d.ts" />
- 
-
-interface Customer {
-
-    firstname    : string;
-
-    lastname     : string;
-
-    age          : number;
-
-    emails       : string[];
-
-    option_a ?   : boolean; // optional
-
-    option_b ?   : boolean; // optional
-
-}
 
 export function index(context) {
-
-    // a customer with invalid data.
-
-    var customer = {
-
-        firstname    : 'dave',
-
-        age          : '33',
-
-        emails       : [12345, 'dave@domain.com', true],
-
-        option_b     : 1,
-
-        option_c     : 1
-    }
-
-    // do validation.
-
-    var errors = context.schema.validate('Customer', customer);
-
-    if(errors) {
-
-        context.response.json(errors);
-    }
+    
+    context.response.json({}); 
 }
 
