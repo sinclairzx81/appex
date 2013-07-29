3251f6807f534755cbf5a7ee7984d4112ed960db
haydn paterson <haydn.developer@gmail.com>
2013-07-29 15:07:21 +1200

    adding sitemap api.

--- program.ts
+++ program.ts
@@ -1,12 +1,55 @@
 ﻿/// <reference path="node_modules/appex/appex.d.ts" />
 
-cascade('index', {verbs:['get', 'post']})
-export function index(context:appex.web.IContext) {
 
-    context.response.send(context.template.render('./app.js'));
+class User {
+
+    public firstname:string;
 }
 
-export function wildcard(context, path) {
+export module admin {
+
+    export function index(context) {
+    
+    }
+
+    export function dashboard(context) {
+        
+
+    }  
+
+    export function modules(xontext){
+    
+    }
+    export function wildcard(context, path, a, b?) {
+
+        context.response.send('not found');    
+    }
+
+    export module users {
+        
+        export function login(context) { }
+
+        export function logout(context) {}
+    }
+}
+
+export module foo {
+
+    export function anm(context) { }
+}
+
+
+cascade('index', {verbs:['get', 'post']})
+export function index(context:appex.web.IContext) {
+    
+    context.response.json( context.sitemap );
+}
+
+export function about(contex) {}
+
+export function contact(contex) {}
+
+export function wildcard(context, path, a, b?) {
 
     context.response.send('not found');    
 
