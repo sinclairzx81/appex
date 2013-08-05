9143bb090c89e7f030b4f8484fabd82fa1bf2788
haydn paterson <haydn.developer@gmail.com>
2013-08-05 15:05:02 +1200

    updates to vm

--- program.ts
+++ program.ts
@@ -1,22 +1,27 @@
 ﻿/// <reference path="node_modules/appex/appex.d.ts" />
 
-declare var cascade;
 
 cascade({website:'http://mysite.com/'}) // global
 
-cascade('index', {title:'home page'})
 export function index(context) {
+
+    console.log(context.cascade)
 
 	context.response.send('index')
 }
 
-cascade('about', {title: 'about page'})
 export function about(context) {
+
 	context.response.send('about')
 }
 
-cascade('sitemap', {title: 'sitemap page'})
 export function sitemap(context) {
-	context.response.json(context.sitemap)
+
+	context.response.send('asd')
+}
+
+export function wildcard(context) {
+
+    context.response.send('not found')
 }
 
