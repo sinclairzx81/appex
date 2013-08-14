31428e26c3d2758e9b7a0449eb4e5f8785f1e3fe
haydn paterson <haydn.developer@gmail.com>
2013-08-15 00:56:12 +1200

    updates to logging. updates to template context

--- build.js
+++ build.js
@@ -16,7 +16,7 @@
 //
 //  for declarations, run the following from the project root:
 //
-//  tsc -out node_modules/appex/index.js node_modules/appex/index.ts -declaration -nolib -comments
+//  tsc -out node_modules/appex/index.js node_modules/appex/index.ts -declaration -noLib
 //
 /////////////////////////////////////////////////////////////////////
 
